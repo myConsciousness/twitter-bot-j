88a84853924d5d5d03a12381bbb147a0da54fc23
Kato Shinya <kato.shinya.dev@gmail.com>
2021-07-24 22:23:50 +0900

    update for the issue (#62)

--- app/src/main/java/org/thinkit/bot/twitter/batch/catalog/TaskType.java
+++ app/src/main/java/org/thinkit/bot/twitter/batch/catalog/TaskType.java
@@ -54,6 +54,11 @@ public enum TaskType implements Catalog<TaskType> {
     AUTO_TWEET_PR(3),
 
     /**
+     * The auto tweet weekly report
+     */
+    AUTO_TWEET_WEEKLY_REPORT(4),
+
+    /**
      * The auto show user
      */
     AUTO_SHOW_USER(100),
